f04c2733a65e7e89b66eade5cdc9ae5701b2d73b
TahaAshfaq <ssc.taha.ashfaq.85@gmail.com>
2024-11-25 01:58:27 +0500

    #

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,30 +1,79 @@
 "use client"; // Add this directive at the top
 import styles from "../Styles/footer.css";
-import homet2 from "../media/homet2.svg";
+import pic1 from "../media/mylogo.svg";
+import pic2 from "../media/Dribble.svg";
+import pic3 from "../media/Email.svg";
+import pic4 from "../media/Insta.svg";
+import pic5 from "../media/Github.svg";
+import pic6 from "../media/Linkedin.svg";
+import pic7 from "../media/location.svg";
+import pic8 from "../media/phone.svg";
 
 import Image from "next/image";
 
 export default function Footer() {
   return (
-    // <div className="foot">
-    // <p className="footerhead">There Is <span style={{
-    //     color: "#8484FF",
-    // }}>Greatness</span>
-    // In Simplicity</p>
-    // </div>
     <div className="foot">
-        <p className="footerhead">There Is <span style={{
-        color: "#8484FF",
-    }}>Greatness</span>
-    <br />
-    In Simplicity</p>
-    <p className="foothead2">Let’s get in contact & work together!!!</p>
-    <div className="footcontent">
-        <Image></Image>
-        <div className="socials"></div>
-        <div className="contacts"></div>
+      <p className="footerhead">
+        There Is{" "}
+        <span
+          style={{
+            color: "#8484FF",
+          }}
+        >
+          Greatness
+        </span>
+        <br />
+        In Simplicity
+      </p>
+      <p className="foothead2">Let’s get in contact & work together!!!</p>
+      <div className="footcontent">
+        <Image src={pic1} alt="dsfklsjdfkj" width={200} height={200} />
+        <div className="socials">
+          <div className="singlesocial">
+            <Image src={pic2} alt="dsfklsjdfkj" width={20} height={20} />
+            <p className="footerp">DRIBBBLE</p>
+          </div>
+          <div className="singlesocial">
+            <Image src={pic6} alt="dsfklsjdfkj" width={20} height={20} />{" "}
+            <p className="footerp">LINKEDIN</p>
+          </div>
+          <div className="singlesocial">
+            <Image src={pic5} alt="dsfklsjdfkj" width={20} height={20} />
+            <p className="footerp">GITHUB</p>
+          </div>
+          <div className="singlesocial">
+            <Image src={pic4} alt="dsfklsjdfkj" width={20} height={20} />
+            <p className="footerp">INSTAGRAM</p>
+          </div>
+        </div>
+        <div className="contacts">
+          <div className="singlecontact">
+            <div className="contactupper">
+              {" "}
+              <Image src={pic8} alt="dsfklsjdfkj" width={20} height={20} />
+              <p className="contactpupper">Phone</p>
+            </div>
+            <p className="contactpdown">0309 0109962</p>
+          </div>
+          <div className="singlecontact">
+            <div className="contactupper">
+              {" "}
+              <Image src={pic7} alt="dsfklsjdfkj" width={20} height={20} />
+              <p className="contactpupper">Location</p>
+            </div>
+            <p className="contactpdown">Margalla Town, Islamabad</p>
+          </div>
+          <div className="singlecontact">
+            <div className="contactupper">
+              {" "}
+              <Image src={pic3} alt="dsfklsjdfkj" width={20} height={20} />
+              <p className="contactpupper">Email</p>
+            </div>
+            <p className="contactpdown">tahaashfaqmalik@gmail.com</p>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-    
   );
 }
--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -7,6 +7,22 @@ import myservices from "../media/myservices.js";
 import Image from "next/image";
 
 export default function Services() {
+  document.querySelectorAll(".viewproject").forEach((button) => {
+    button.addEventListener("mousemove", (e) => {
+      const rect = button.getBoundingClientRect();
+      const x = ((e.clientX - rect.left) / rect.width) * 100;
+      const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+      button.style.setProperty("--mouse-x", `${x}%`);
+      button.style.setProperty("--mouse-y", `${y}%`);
+    });
+
+    button.addEventListener("mouseleave", () => {
+      button.style.setProperty("--mouse-x", `50%`); /* Reset to center */
+      button.style.setProperty("--mouse-y", `50%`);
+    });
+  });
+
   // Correcting the map function
   const myservicescard = myservices.map((singlecard) => {
     return (
@@ -23,7 +39,8 @@ export default function Services() {
     <div className="aboutme">
       <p className="head">My Services</p>
       <p className="servicescontent">
-        My services blend creativity and strategy to craft intuitive user experiences
+        My services blend creativity and strategy to craft intuitive user
+        experiences
       </p>
       <div className="myservices">
         {myservicescard} {/* Render the mapped ServiceCard components */}
