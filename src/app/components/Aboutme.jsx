f623650e8b0654829a60cd6774e744b111750322
TahaAshfaq <ssc.taha.ashfaq.85@gmail.com>
2025-02-14 12:53:51 +0500

    #

--- src/app/components/Aboutme.jsx
+++ src/app/components/Aboutme.jsx
@@ -60,9 +60,10 @@ export default function Aboutme() {
               <span
                 key={index}
                 style={{
-                  transition: "color 0.7s ease",
-                  color: shouldHighlight ? "#33336B" : "#E5E5FB",
-                  display: "inline",
+                  transition: "background-color 0.7s ease",
+                  backgroundColor: shouldHighlight ? "#CEE4FF" : "transparent",
+                  display: "inline-block",
+                  padding: "0px 5px"
                 }}
               >
                 {word}{" "}
