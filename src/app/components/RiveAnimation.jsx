e6df410ffa15d95a74a78fccd7443aa07c5ed48e
TahaAshfaq <ssc.taha.ashfaq.85@gmail.com>
2025-02-21 12:58:42 +0500

    rive responsive

--- src/app/components/RiveAnimation.jsx
+++ src/app/components/RiveAnimation.jsx
@@ -3,14 +3,53 @@ import { useRive } from "@rive-app/react-canvas";
 import style from "../Styles/rive.css";
 
 export default function RiveAnimation() {
-  const { rive, RiveComponent } = useRive({
+  const { rive: rive, RiveComponent: RiveComponent  } = useRive({
+    src: "/rivemobile.riv",
+    autoplay: true,
+  });
+  const { rive: rive2, RiveComponent: RiveComponent2 } = useRive({
     src: "/rive.riv",
     autoplay: true,
   });
 
   return (
+    <>
+    <div className="rive2">
+      <RiveComponent2 className="rive-animation2" />
+    </div>
     <div className="rive">
       <RiveComponent className="rive-animation" />
     </div>
+  </>
   );
 }
+
+// "use client";
+// import { useRive } from "@rive-app/react-canvas";
+// import style from "../Styles/rive.css";
+
+// export default function RiveAnimation() {
+//   const { rive: rive, RiveComponent: Rive1Component } = useRive({
+//     src: "/rive.riv", // First animation
+//     autoplay: true,
+//   });
+
+//   const { rive: rivemobile, RiveComponent: Rive2Component } = useRive({
+//     src: "/rivemobile.riv", // Second animation
+//     autoplay: true,
+//   });
+
+//   return (
+//     <>
+//       {/* First Animation */}
+//       <div className="rive">
+//         <Rive1Component className="rive-animation" />
+//       </div>
+
+//       {/* Second Animation */}
+//       <div className="rivemobile">
+//         <Rive2Component className="rive-animation-mobile" />
+//       </div>
+//     </>
+//   );
+// }
