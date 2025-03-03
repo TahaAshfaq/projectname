8ac0a1d380856de777129d4d6e9081042200432a
TahaAshfaq <ssc.taha.ashfaq.85@gmail.com>
2025-03-03 10:45:12 +0500

    #

--- src/app/components/RiveAnimation.jsx
+++ src/app/components/RiveAnimation.jsx
@@ -1,55 +1,25 @@
-"use client";
-import { useRive } from "@rive-app/react-canvas";
-import style from "../Styles/rive.css";
-
-export default function RiveAnimation() {
-  const { rive: rive, RiveComponent: RiveComponent  } = useRive({
-    src: "/rivemobile.riv",
-    autoplay: true,
-  });
-  const { rive: rive2, RiveComponent: RiveComponent2 } = useRive({
-    src: "/rive.riv",
-    autoplay: true,
-  });
-
-  return (
-    <>
-    <div className="rive2">
-      <RiveComponent2 className="rive-animation2" />
-    </div>
-    <div className="rive">
-      <RiveComponent className="rive-animation" />
-    </div>
-  </>
-  );
-}
-
 // "use client";
 // import { useRive } from "@rive-app/react-canvas";
 // import style from "../Styles/rive.css";
 
 // export default function RiveAnimation() {
-//   const { rive: rive, RiveComponent: Rive1Component } = useRive({
-//     src: "/rive.riv", // First animation
+//   const { rive: rive, RiveComponent: RiveComponent  } = useRive({
+//     src: "/rivemobile.riv",
 //     autoplay: true,
 //   });
-
-//   const { rive: rivemobile, RiveComponent: Rive2Component } = useRive({
-//     src: "/rivemobile.riv", // Second animation
+//   const { rive: rive2, RiveComponent: RiveComponent2 } = useRive({
+//     src: "/rive.riv",
 //     autoplay: true,
 //   });
 
 //   return (
 //     <>
-//       {/* First Animation */}
-//       <div className="rive">
-//         <Rive1Component className="rive-animation" />
-//       </div>
-
-//       {/* Second Animation */}
-//       <div className="rivemobile">
-//         <Rive2Component className="rive-animation-mobile" />
-//       </div>
-//     </>
+//     <div className="rive2">
+//       <RiveComponent2 className="rive-animation2" />
+//     </div>
+//     <div className="rive">
+//       <RiveComponent className="rive-animation" />
+//     </div>
+//   </>
 //   );
 // }
